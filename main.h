03d9d44ad2fe2e3c9d53a69e6cc96608ed3c8c06
Felix Yeboah <99273714+fellydyll@users.noreply.github.com>
2022-04-19 20:23:36 +0100

    Update main.h

--- main.h
+++ main.h
@@ -11,3 +11,17 @@
 #include <unistd.h>
 
 #define BUFFSIZE 1024
+
+/**
+ * struct convert - definesa struct for symbols and functions
+ *
+ * @sym: operator
+ * @f: function associated with sum
+ */
+
+typedef struct convert
+{
+	char *sym;
+	int (*f)(va_list);
+} convert_t;
+
