f74d3beca66a75dedf7273c377810ad0cf3f5c5c
Felix Yeboah <99273714+fellydyll@users.noreply.github.com>
2022-04-19 20:23:09 +0100

    Create main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef PRINTF
+#define PRINTF
+
+/* include default libraries */
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <unistd.h>
+
+#define BUFFSIZE 1024
